8a4292a852f87781ccefda009b9b4a52f86e0a89
Danilo Souza <danilo.souza@edu.unipar.br>
2022-02-03 22:43:33 -0300

    Adicionado tela de detalhes e carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/route_manager.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:vakinha_burger_mobile/app/core/bindings/application_binding.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha_burger_mobile/app/routes/auth_routers.dart';
+import 'package:vakinha_burger_mobile/app/routes/home_routers.dart';
+import 'package:vakinha_burger_mobile/app/routes/product_routers.dart';
 import 'package:vakinha_burger_mobile/app/routes/splash_routers.dart';
 
-void main() {
+Future<void> main() async {
+  await GetStorage.init();
   runApp(const VakinhaBurgerMainApp());
 }
 
-class VakinhaBurgerMainApp extends StatefulWidget {
+class VakinhaBurgerMainApp extends StatelessWidget {
   const VakinhaBurgerMainApp({Key? key}) : super(key: key);
 
-  @override
-  _VakinhaBurgerMainAppState createState() => _VakinhaBurgerMainAppState();
-}
-
-class _VakinhaBurgerMainAppState extends State<VakinhaBurgerMainApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Vakinha Burger',
       theme: VakinhaUI.theme,
+      initialBinding: ApplicationBinding(),
       getPages: [
         ...SplashRouters.routers,
         ...AuthRouters.routers,
+        ...HomeRouters.routers,
+        ...ProductRouters.routers,
       ],
     );
   }
